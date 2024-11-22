2352c6d62fe9cc5f25a6a8cd23878bc4aa272b5f
Ognjen Stanić <ognjen@thrivea.com>
2024-11-22 14:50:57 +0100

    More test cases, refactor.

--- Game.Rpsls.Specs/Specs.cs
+++ Game.Rpsls.Specs/Specs.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Xunit;
+using static Game.Rpsls.Specs.HandGesture;
 
 namespace Game.Rpsls.Specs;
 
@@ -13,19 +14,18 @@ public class Specs
         _player2.PrepareGesture(player2Gesture);
         _game.Play();
 
-        _game.Winner.Should().NotBeNull();
-        _game.Winner.Name.Should().Be(_player1.Name);
+        _game.WinnerName.Should().Be(_player1.Name);
     }
 
     [Theory]
     [MemberData(nameof(NoWinnerCombos))]
-    public void No_winner(HandGesture player1Gesture, HandGesture player2Gesture)
+    public void No_winner_rules(HandGesture player1Gesture, HandGesture player2Gesture)
     {
         _player1.PrepareGesture(player1Gesture);
         _player2.PrepareGesture(player2Gesture);
         _game.Play();
 
-        _game.Winner.Should().BeNull();
+        _game.WinnerName.Should().BeNull();
     }
 
     #region Specs setup
@@ -44,13 +44,18 @@ public class Specs
 
     public static IEnumerable<object[]> WinningCombos()
     {
-        foreach (var (player1Gesture, player2Gesture) in Game.Rules) yield return [player1Gesture, player2Gesture];
+        foreach (var (player1Gesture, player2Gesture) in Game.Rules)
+        {
+            yield return [player1Gesture, player2Gesture];
+        }
     }
 
     public static IEnumerable<object[]> NoWinnerCombos()
     {
-        yield return [HandGesture.Rock, HandGesture.Rock];
-        yield return [HandGesture.Scissors, HandGesture.Scissors];
+        foreach (var handGesture in Enum.GetValues(typeof(HandGesture)))
+        {
+            yield return [handGesture, handGesture];
+        }
     }
 
     #endregion
@@ -60,34 +65,42 @@ public class Game(Player player1, Player player2)
 {
     public static readonly IEnumerable<(HandGesture, HandGesture)> Rules =
     [
-        (HandGesture.Scissors, HandGesture.Paper), // Scissors cuts Paper
-        (HandGesture.Paper, HandGesture.Rock), // Paper covers Rock
-        (HandGesture.Rock, HandGesture.Lizard), // Rock crushes Lizard
-        (HandGesture.Lizard, HandGesture.Spock), // Lizard poisons Spock
-        (HandGesture.Spock, HandGesture.Scissors), // Spock smashes Scissors
-        (HandGesture.Scissors, HandGesture.Lizard), // Scissors decapitates Lizard
-        (HandGesture.Lizard, HandGesture.Paper), // Lizard eats Paper
-        (HandGesture.Paper, HandGesture.Spock), // Paper disproves Spock
-        (HandGesture.Spock, HandGesture.Rock), // Spock vaporizes Rock
-        (HandGesture.Rock, HandGesture.Scissors) // Rock crushes Scissors
+        (Scissors, Paper), // Scissors cuts Paper
+        (Paper, Rock), // Paper covers Rock
+        (Rock, Lizard), // Rock crushes Lizard
+        (Lizard, Spock), // Lizard poisons Spock
+        (Spock, Scissors), // Spock smashes Scissors
+        (Scissors, Lizard), // Scissors decapitates Lizard
+        (Lizard, Paper), // Lizard eats Paper
+        (Paper, Spock), // Paper disproves Spock
+        (Spock, Rock), // Spock vaporizes Rock
+        (Rock, Scissors) // Rock crushes Scissors
     ];
 
-    public Player Winner { get; private set; }
+    private Player Winner { get; set; }
+
+    public string WinnerName
+    {
+        get => Winner?.Name;
+    }
 
     public void Play()
     {
         if (Rules.Any(rule => rule.Item1 == player1.ChosenGesture && rule.Item2 == player2.ChosenGesture))
+        {
             Winner = player1;
+        }
+
         if (Rules.Any(rule => rule.Item1 == player2.ChosenGesture && rule.Item2 == player1.ChosenGesture))
+        {
             Winner = player2;
+        }
     }
 }
 
-public class Player(string name)
+public record Player(string Name)
 {
     public HandGesture ChosenGesture { get; private set; }
-
-    public string Name => name;
 
     public void PrepareGesture(HandGesture gesture)
     {
